318ea68058843935a106f6b6e369e4dd0ae31dce
febworker <143534914+febworker@users.noreply.github.com>
2024-01-28 23:46:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-from setuptools import setup
-setup(
-    name="clean_folder",
-    version="1.0",
-    packages=["clean_folder"],
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "clean-folder=clean_folder.clean:main"
-        ],
-    },
-)
+from setuptools import setup, find_namespace_packages
+
+setup(name='remind_me',
+    version='1.0.0',
+    description='contact_assistant, sort_direcrory_function',
+    author='Svitlana Shulha, Roman Gryshko, Oksana Horishna',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['contact=remind_me.main:run']}
+    ) 
